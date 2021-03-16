5ff1995c02c569ec76dcbf0e50bdc03a39247965
Michael Cronin <mcronin02@gmail.com>
2021-03-15 23:41:34 -0400

    Update mochawesome-merge.js

--- bin/mochawesome-merge.js
+++ bin/mochawesome-merge.js
@@ -19,6 +19,12 @@ yargs
     describe: 'Output file path',
     type: 'string',
   })
+  .option('s', {
+    alias: 'stats',
+    demandOption: false,
+    describe: 'Stats file path',
+    type: 'string',
+  })
   .option('f', {
     alias: 'files',
     demandOption: false,
@@ -27,11 +33,35 @@ yargs
   })
   .help()
 
-const { files, output } = yargs.argv
+const { files, output, stats } = yargs.argv
+
+function formatDuration(duration) {
+  const seconds = duration / 1000;
+  return seconds.toString();
+}
 
 merge({ files }).then(
   report => {
     const content = JSON.stringify(report, null, 2)
+    const rawStats = report.stats;
+    rawStats.status = "FAILED";
+    rawStats.status = report.stats.passPercent > 0 && report.stats.passPercent < 100 ? "PARTIAL FAILURE" : rawStats.status;
+    rawStats.status = report.stats.passPercent === 100 ? "SUCCESS" : rawStats.status;
+    rawStats.testsRegistered = rawStats.testsRegistered.toString();
+    rawStats.passes = rawStats.passes.toString();
+    rawStats.failures = rawStats.failures.toString();
+    rawStats.passPercent = Math.round(rawStats.passPercent,0).toString();
+    rawStats.skipped = rawStats.skipped.toString();
+    rawStats.prettyDuration = formatDuration(rawStats.duration);
+    rawStats.reportUrl = "test url";
+    const statsContent = JSON.stringify(rawStats, null, 2);
+  
+    if (stats) {
+      const statsFilePath = path.resolve(process.cwd(), stats)
+      fs.mkdirSync(path.dirname(statsFilePath), { recursive: true })
+      fs.writeFileSync(statsFilePath, statsContent, { flag: 'w' })
+    }
+
     if (output) {
       const outputFilePath = path.resolve(process.cwd(), output)
       fs.mkdirSync(path.dirname(outputFilePath), { recursive: true })
